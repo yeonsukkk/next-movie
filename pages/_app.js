29243cff1a7885e2b3a802b007fe99478437a21e
yeonsukkk <dustn910720@gmail.com>
2023-03-27 22:51:35 +0900

    css파일 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import 'reset-css';
 import '@/styles/globals.css'
+import '@/styles/layout.css'
+import '@/styles/media.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
