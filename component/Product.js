8097aea57a6c708f8505447f9045fc1817ffbc8e
yeonsukkk <dustn910720@gmail.com>
2023-03-27 22:47:39 +0900

    메인페이지 영화리스트

--- component/Product.js
+++ component/Product.js
@@ -1,11 +1,38 @@
-import { ProductItem } from "./ProductItem"
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import Image from 'next/image';
+import Link from 'next/link';
 
-export const Product = () => {
+export const Product = ({movie}) => {
   return(
-    <section>
-      <h2 className="hide">영화 목록</h2>
-      Product
-      <ProductItem />
+    <section className="movieWrap">
+      <h2>인기있는 영화 목록</h2>
+      <div className="movieCnt">
+        <Swiper
+          slidesPerView={2.5}
+          spaceBetween={15}
+        >
+          {
+            movie?.map(item => (
+              <SwiperSlide key={item.id}>
+                <Link href="/movie/[id]" as={`/movie/${item.id}`}>
+                  <figure>
+                    <Image
+                      src={item.img}
+                      alt={item.title}
+                      width={500}
+                      height={300}
+                      unoptimized={true}
+                    />
+                    <figcaption>{item.title}</figcaption>
+                  </figure>
+                </Link>
+              </SwiperSlide>
+            ))
+          }
+        </Swiper>
+      </div>
     </section>
   )
 }
