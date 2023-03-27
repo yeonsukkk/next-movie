c3542db98d05d6a02a6342413894fe49705a398f
yeonsukkk <dustn910720@gmail.com>
2023-03-27 22:34:10 +0900

    메인비주얼 영역 코딩

--- component/MainVisual.js
+++ component/MainVisual.js
@@ -1,8 +1,44 @@
-export const MainVisual = () => {
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Autoplay, EffectFade, Pagination } from 'swiper';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
+SwiperCore.use([Autoplay, Pagination, EffectFade]);
+
+import Image from "next/image"
+
+export const MainVisual = ({mainvisual}) => {
+  // console.log(mainvisual)
   return(
-    <section>
+    <section className="mainVisualWrap">
       <h2 className="hide">메인비주얼 영역</h2>
-      MainVisual
+      <Swiper
+        modules={[Autoplay, Pagination, EffectFade]}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false
+        }}
+        speed={1000}
+        loop={true}
+        pagination={{clickable: true}}
+        effect={"fade"}
+      >
+        {
+          mainvisual?.map(item => (
+            <SwiperSlide key={item.id}>
+              <Image
+                src={item.imgUrl}
+                alt={item.title}
+                width={500}
+                height={300}
+                unoptimized={true}
+              />
+            </SwiperSlide>
+          ))
+        }
+      </Swiper>
     </section>
   )
 }
+
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,9 @@ import { MainVisual } from "@/component/MainVisual";
 import { Nav } from "@/component/Nav";
 import { Product } from "@/component/Product";
 import Head from "next/head";
+import { genreList } from "./api/api";
 
-export default function Home() {
+export default function Home(props) {
   return (
     <>
       <Layout>
@@ -13,12 +14,29 @@ export default function Home() {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <Nav />
-        <main>
-          <MainVisual />
-          <Product />
-        </main>
+        <div className="allWrap">
+          <Nav />
+          <main className="cntWrap">
+            <MainVisual mainvisual={props.mainvisual} />
+            <Product movie={props.movieData} genreList={genreList} />
+          </main>
+        </div>
       </Layout>
     </>
   )
+}
+
+export async function getStaticProps(){
+  const res = await fetch('https://ys-next-movie.s3.ap-northeast-2.amazonaws.com/json/mainVisual.json')
+  const data = await res.json()
+
+  const movieRes = await fetch('https://ys-next-movie.s3.ap-northeast-2.amazonaws.com/json/movieData.json')
+  const movieData = await movieRes.json()
+
+  return {
+    props: {
+      mainvisual: data,
+      movieData
+    }
+  }
 }
