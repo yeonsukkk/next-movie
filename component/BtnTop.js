79f82f4e29a9272e2367681ee13183bdcbcfef17
yeonsukkk <dustn910720@gmail.com>
2023-03-28 16:01:45 +0900

    탑 버튼 기능 구현

--- component/BtnTop.js
+++ component/BtnTop.js
@@ -3,7 +3,13 @@ import { RxPinTop } from 'react-icons/rx';
 export const BtnTop = () => {
   return(
     <>
-      <button type="button" className="btnTop"><RxPinTop /></button>
+      <button
+        type="button"
+        className="btnTop"
+        onClick={() => window.scrollTo({top: 0, left: 0, behavior: 'smooth'})}
+      >
+        <RxPinTop />
+      </button>
     </>
   )
 }
