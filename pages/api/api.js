a660f0087a9a32bf8629add4ea7ca866b742e290
yeonsukkk <dustn910720@gmail.com>
2023-03-28 17:07:56 +0900

    날짜 바꾸기 함수 추가

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,1 +1,6 @@
 export const genreList = ["애니메이션", "액션", "어드벤처","환타지", "코미디", "드라마", "미스터리", "스릴러"]
+
+export const changeDate = (getDate) => {
+  const d = new Date(getDate)
+  return `${d.getFullYear()}.${d.getMonth()+1}.${d.getDate()}`
+}
